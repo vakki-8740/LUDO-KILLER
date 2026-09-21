0424e3a9834be42c5b9ce3fbae43aab131088eba
vakki-8740 <lightsir8740@gmail.com>
2026-09-21 22:52:00 +0530

    chore: Remove zerotize-dashboard project

--- zerotize-dashboard/src/routes.js
+++
@@ -1,6 +0,0 @@
-export const HOME = "/";
-export const LINKS = "/links";
-export const CHECKOUT = "/checkout";
-export const SUPPORT = "/tickets";
-export const PROFILE = "/profile";
-export const API_CREDENTIALS = "/api";
