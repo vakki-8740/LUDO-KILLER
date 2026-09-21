ffd53e7261ca79703450270cc1ef93deb83723ed
vakki-8740 <lightsir8740@gmail.com>
2026-09-21 22:45:46 +0530

    fix: Import React in App.jsx

--- zerotize-dashboard/src/App.jsx
+++ zerotize-dashboard/src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { HOME, LINKS, CHECKOUT, SUPPORT, PROFILE, API_CREDENTIALS } from "./routes";
